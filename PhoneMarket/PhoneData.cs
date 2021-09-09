52c8d369d5aa54ad08fbb75a1d04682a1475c4d1
MeasureUP <MeasureUP@PC-1130>
2021-09-09 15:34:22 +0200

    Add project files.

---
+++ PhoneMarket/PhoneData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneMarket
+{
+    public class PhoneData
+    {
+        public static List<PhoneInfo> Phones = new List<PhoneInfo>();
+
+        public static void GenerateList()
+        {
+            Phones.Add(new PhoneInfo("Huawei", "P30", "6.47\" FHD + (2340x1080) OLED,\n Kirin 980 Octa - Core(2x Cortex - A76 2.6GHz + 2x Cortex - A76 1.92GHz + 4x Cortex - A55 1.8GHz),\n 8GB RAM, 128GB ROM, 40 + 20 + 8 + TOF / 32MP,\n Dual SIM,\n 4200mAh,\n Android 9.0 + EMUI 9.1", 697));
+            Phones.Add(new PhoneInfo("Samsung", "Galaxy A52", "64 megapixel camera,\n 4k videokwaliteit6.5 inch AMOLED scherm128 GB opslaggeheugen (Uitbreidbaar met Micro-sd)Water-en stofbestendig (IP67)", 399));
+            Phones.Add(new PhoneInfo("Apple", "Iphone11", "Met de dubbele camera schiet je in elke situatie een perfecte foto of videoDe krachtige A13-chipset zorgt voor razendsnelle prestatiesMet Face ID hoef je enkel en alleen naar je toestel te kijken om te ontgrendelenHet toestel heeft een lange accuduur dankzij een energiezuinige processor", 619));
+            Phones.Add(new PhoneInfo("Google", "Pixel 4a", "12.2 megapixel camera, 4k videokwaliteit5.81 inch OLED scherm128 GB opslaggeheugen 3140 mAh accucapaciteit", 411));
+            Phones.Add(new PhoneInfo("Xiaomi", "Redmi Note 10 Pro", "108 megapixel camera, 4k videokwaliteit6.67 inch AMOLED scherm128 GB opslaggeheugen (Uitbreidbaar met Micro-sd)Water-en stofbestendig (IP53)", 298));
+        }
+        public struct PhoneInfo
+        {
+            public string TMerk;
+            public string TType;
+            public string TDescription;
+            public double TPrice;
+
+            public PhoneInfo(string TMerk, string TType, string TDescription, double TPrice)
+            {
+                this.TMerk = TMerk;
+                this.TType = TType;
+                this.TDescription = TDescription;
+                this.TPrice = TPrice;
+            }
+        }
+    }
+}
---
+++ PhoneMarket/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using PhoneMarket;
+namespace PhoneMarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PhoneData.GenerateList();
+            MainMenu();
+        }
+
+        static void MainMenu()
+        {
+            Console.WriteLine("Please Choose a Phone Type (By Number)\n");
+            foreach (var item in PhoneData.Phones)
+            {
+                Console.WriteLine(PhoneData.Phones.IndexOf(item) + " " + item.TMerk);
+            }
+
+
+            Console.WriteLine("Press ESC to Exit Application");
+
+
+            var iKey = Console.ReadKey();
+            Console.Clear();
+            if (iKey.Key == ConsoleKey.Escape)
+            {
+                return;
+            }
+            else
+            {
+                try
+                {
+                    MenuSelected(Convert.ToInt32(iKey.KeyChar.ToString()));
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("Error Wrong Input.");
+                }
+                
+            }
+        }
+
+        public static void MenuSelected(int Input)
+        {
+            if(Input >= 0 && Input < PhoneData.Phones.Count)
+            {
+                Console.WriteLine("Merk " + PhoneData.Phones[Input].TMerk + "\n");
+                Console.WriteLine("Type " + PhoneData.Phones[Input].TType + "\n");
+                Console.WriteLine("Info " + PhoneData.Phones[Input].TDescription + "\n");
+                Console.WriteLine("Prijs " + PhoneData.Phones[Input].TPrice + "\n");
+            }
+            var iKey = Console.ReadKey();
+            if(iKey.Key == ConsoleKey.Escape)
+            {
+                return;
+            }else if(iKey.Key == ConsoleKey.Enter || iKey.Key == ConsoleKey.Backspace)
+            {
+                Console.Clear();
+                MainMenu();
+            }
+        }
+    }
+}
